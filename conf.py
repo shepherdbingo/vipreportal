bfecaba176b9e13db418ceca0d63db0ec2b4e895
shepherdbingo <shepherdbingo@protonmail.com>
2025-12-23 22:32:13 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "to Log In to Dashlane: Step-by-Step Guide to Access and Manage Your Account Securely"
+html_title = "to Log In to Your VIPRE Account: Sign Into Your Antivirus or Internet Security Account on PC and Recover Passwords Easily"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
